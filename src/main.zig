8ce28a1d376e80222d846296d26c34d844bf1763
Darren Fielding <darren.fielding@hotmail.co.uk>
2023-08-27 15:52:17 +0100

    Basic file drop functionality

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,31 @@ const rl = @cImport({
 });
 
 pub fn main() !void {
-    rl.InitWindow(800, 800, "Nonogram-Zig");
+    rl.InitWindow(800, 800, "File Drop");
     defer rl.CloseWindow();
 
+    var filepath: [2048]u8 = undefined;
+    var has_file: bool = false;
+
     while (!rl.WindowShouldClose()) {
+        // Detect file
+        if (rl.IsFileDropped()) {
+            var dropped: rl.FilePathList = rl.LoadDroppedFiles();
+            defer rl.UnloadDroppedFiles(dropped);
+            @memcpy(&filepath, @as([*]u8, @ptrCast(&(dropped.paths.*[0]))));
+            has_file = true;
+        }
+
+        // Draw
         rl.BeginDrawing();
         rl.ClearBackground(rl.DARKGRAY);
-        rl.DrawText("Nonogram", 300, 380, 40, rl.LIGHTGRAY);
+
+        if (!has_file) {
+            rl.DrawText("Drop file here", 200, 380, 20, rl.LIGHTGRAY);
+        } else {
+            rl.DrawText(@as([*c]const u8, &filepath), 10, 380, 20, rl.LIGHTGRAY);
+        }
+
         rl.EndDrawing();
     }
 }
