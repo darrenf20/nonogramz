6c2b9315aba4cc2db3333ecdfae7a2317b66ff0a
Darren Fielding <darren.fielding@hotmail.co.uk>
2023-09-03 19:03:34 +0100

    Rough drawing of number text

--- src/main.zig
+++ src/main.zig
@@ -58,8 +58,10 @@ const Puzzle_Data = struct {
 
     size: c_int,
     gap: c_int,
-    w_offset: c_int = 0,
-    h_offset: c_int = 0,
+    x_offset: c_int = 0,
+    y_offset: c_int = 0,
+    row_strs: [][:0]const u8 = undefined,
+    col_strs: [][][:0]const u8 = undefined,
 
     fn init(self: *Puzzle_Data) !void {
         self.deinit();
@@ -85,6 +87,8 @@ const Puzzle_Data = struct {
 
         self.row_info = try self.allocator.alloc([]usize, row_len);
         self.col_info = try self.allocator.alloc([]usize, col_len);
+        self.row_strs = try self.allocator.alloc([:0]const u8, row_len);
+        self.col_strs = try self.allocator.alloc([][:0]const u8, col_len);
 
         self.grid = try self.allocator.alloc([]u1, row_len);
         for (self.grid) |*row| {
@@ -93,23 +97,27 @@ const Puzzle_Data = struct {
         }
 
         _ = iterator.next().?; // skip blank line
-        for (self.col_info) |*line| {
+        for (self.col_info, 0..) |*line, i| {
             var str: []const u8 = iterator.next().?;
             num_it = std.mem.splitScalar(u8, str, ' ');
 
             var len = std.mem.count(u8, str, " ") + 1;
             line.* = try self.allocator.alloc(usize, len);
 
-            for (line.*) |*sq| {
-                sq.* = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
+            self.col_strs[i] = try self.allocator.alloc([:0]const u8, len);
+
+            for (line.*, 0..) |*sq, j| {
+                const num = num_it.next().?;
+                sq.* = try std.fmt.parseUnsigned(usize, num, 10);
+                self.col_strs[i][j] = try self.allocator.dupeZ(u8, num);
             }
 
             var offset = @as(c_int, @intCast(len)) * (self.size + self.gap);
-            if (offset > self.h_offset) self.h_offset = offset;
+            if (offset > self.y_offset) self.y_offset = offset;
         }
 
         _ = iterator.next().?; // skip blank line
-        for (self.row_info) |*line| {
+        for (self.row_info, 0..) |*line, i| {
             var str: []const u8 = iterator.next().?;
             num_it = std.mem.splitScalar(u8, str, ' ');
 
@@ -120,10 +128,9 @@ const Puzzle_Data = struct {
                 sq.* = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
             }
 
-            const cstr = try self.allocator.dupeZ(u8, str);
-            var offset = rl.MeasureText(cstr, self.size) + self.gap;
-            self.allocator.free(cstr);
-            if (offset > self.w_offset) self.w_offset = offset;
+            self.row_strs[i] = try self.allocator.dupeZ(u8, str);
+            var offset = rl.MeasureText(self.row_strs[i], self.size) + 3 * self.gap;
+            if (offset > self.x_offset) self.x_offset = offset;
         }
     }
 
@@ -141,19 +148,47 @@ const Puzzle_Data = struct {
         for (self.grid) |line| self.allocator.free(line);
         self.allocator.free(self.grid);
 
-        self.w_offset = 0;
-        self.h_offset = 0;
+        self.x_offset = 0;
+        self.y_offset = 0;
+
+        for (self.row_strs) |line| self.allocator.free(line);
+        self.allocator.free(self.row_strs);
+
+        for (self.col_strs) |line| {
+            for (line) |num| self.allocator.free(num);
+            self.allocator.free(line);
+        }
+        self.allocator.free(self.col_strs);
     }
 
     fn draw(self: Puzzle_Data, window_w: usize, window_h: usize) void {
         _ = window_w;
         _ = window_h;
 
-        const x0 = self.w_offset;
-        var y = self.h_offset;
+        var x: c_int = self.x_offset + 4 * self.gap;
+        var y: c_int = 0;
 
+        // Draw column number text
+        for (self.col_strs) |line| {
+            y = 0;
+            for (line) |num| {
+                rl.DrawText(num.ptr, x, y, self.size, rl.WHITE);
+                y += self.size + self.gap;
+            }
+            x += self.size + self.gap;
+        }
+
+        // Draw row number text
+        x = 0;
+        y = self.y_offset + 2 * self.gap;
+        for (self.row_strs) |line| {
+            rl.DrawText(line, x, y, self.size, rl.WHITE);
+            y += self.size + self.gap;
+        }
+
+        y = self.y_offset;
         for (self.grid, 1..) |row, i| {
-            var x = x0;
+            x = self.x_offset;
             for (row, 1..) |sq, j| {
                 const colour = if (sq == 0) rl.WHITE else rl.BLACK;
                 rl.DrawRectangle(x, y, self.size, self.size, colour);
