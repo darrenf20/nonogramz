da756f4187dd155c7bac27d1be67444ac7a58281
Darren Fielding <darren.fielding@hotmail.co.uk>
2023-09-05 15:34:45 +0100

    Use bytes and buffer instead of allocating

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const rl = @cImport({
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    const ally = gpa.allocator();
     defer {
         const status = gpa.deinit();
         if (status == .leak) std.debug.print("* Memory leak detected *\n", .{});
@@ -16,7 +16,7 @@ pub fn main() !void {
 
     const size = @as(c_int, @intCast(@min(win_w, win_h) * 4 / 100));
     const gap: c_int = @divFloor(size, 15);
-    var data = Puzzle_Data{ .allocator = allocator, .size = size, .gap = gap };
+    var data = Data{ .ally = ally, .size = size, .gap = gap };
     defer data.deinit();
 
     rl.InitWindow(win_w, win_h, "Nonogram");
@@ -41,41 +41,39 @@ pub fn main() !void {
                 rl.LIGHTGRAY,
             );
         } else {
-            data.draw(win_w, win_h);
+            data.draw();
         }
 
         rl.EndDrawing();
     }
 }
 
-const Puzzle_Data = struct {
-    allocator: std.mem.Allocator,
+const Data = struct {
+    ally: std.mem.Allocator,
 
     bytes: []u8 = &.{},
+    buffer: [256]u8 = undefined,
+
     row_info: [][]usize = undefined,
     col_info: [][]usize = undefined,
     grid: [][]u1 = undefined,
 
+    // try making these usize
     size: c_int,
     gap: c_int,
     x_offset: c_int = 0,
     y_offset: c_int = 0,
-    row_strs: [][:0]const u8 = undefined,
-    col_strs: [][][:0]const u8 = undefined,
 
-    fn init(self: *Puzzle_Data) !void {
+    fn init(self: *Data) !void {
         self.deinit();
 
         var dropped: rl.FilePathList = rl.LoadDroppedFiles();
         const file = try std.fs.openFileAbsoluteZ(
-            @as([*:0]u8, @ptrCast(&(dropped.paths.*[0]))),
+            @as([*:0]u8, @ptrCast(&(dropped.paths.*[0]))), // simplify?
             .{},
         );
         rl.UnloadDroppedFiles(dropped);
-        self.bytes = try file.readToEndAlloc(
-            self.allocator,
-            try file.getEndPos(),
-        );
+        self.bytes = try file.readToEndAlloc(self.ally, try file.getEndPos());
         file.close();
 
         // Populate puzzle info
@@ -85,31 +83,26 @@ const Puzzle_Data = struct {
         var col_len = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
         var row_len = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
 
-        self.row_info = try self.allocator.alloc([]usize, row_len);
-        self.col_info = try self.allocator.alloc([]usize, col_len);
-        self.row_strs = try self.allocator.alloc([:0]const u8, row_len);
-        self.col_strs = try self.allocator.alloc([][:0]const u8, col_len);
+        self.row_info = try self.ally.alloc([]usize, row_len);
+        self.col_info = try self.ally.alloc([]usize, col_len);
 
-        self.grid = try self.allocator.alloc([]u1, row_len);
+        // Zero-initialise grid
+        self.grid = try self.ally.alloc([]u1, row_len);
         for (self.grid) |*row| {
-            row.* = try self.allocator.alloc(u1, col_len);
+            row.* = try self.ally.alloc(u1, col_len);
             for (row.*) |*square| square.* = 0;
         }
 
         _ = iterator.next().?; // skip blank line
-        for (self.col_info, 0..) |*line, i| {
+        for (self.col_info) |*line| {
             var str: []const u8 = iterator.next().?;
             num_it = std.mem.splitScalar(u8, str, ' ');
 
             var len = std.mem.count(u8, str, " ") + 1;
-            line.* = try self.allocator.alloc(usize, len);
+            line.* = try self.ally.alloc(usize, len);
 
-            self.col_strs[i] = try self.allocator.alloc([:0]const u8, len);
-
-            for (line.*, 0..) |*sq, j| {
-                const num = num_it.next().?;
-                sq.* = try std.fmt.parseUnsigned(usize, num, 10);
-                self.col_strs[i][j] = try self.allocator.dupeZ(u8, num);
+            for (line.*) |*sq| {
+                sq.* = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
             }
 
             var offset = @as(c_int, @intCast(len)) * (self.size + self.gap);
@@ -117,75 +110,72 @@ const Puzzle_Data = struct {
         }
 
         _ = iterator.next().?; // skip blank line
-        for (self.row_info, 0..) |*line, i| {
+        for (self.row_info) |*line| {
             var str: []const u8 = iterator.next().?;
             num_it = std.mem.splitScalar(u8, str, ' ');
 
             var len = std.mem.count(u8, str, " ") + 1;
-            line.* = try self.allocator.alloc(usize, len);
+            line.* = try self.ally.alloc(usize, len);
 
             for (line.*) |*sq| {
                 sq.* = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
             }
 
-            self.row_strs[i] = try self.allocator.dupeZ(u8, str);
-            var offset = rl.MeasureText(self.row_strs[i], self.size) + 3 * self.gap;
+            const cstr = self.bufZ(str);
+            var offset = rl.MeasureText(cstr, self.size) + 3 * self.gap;
             if (offset > self.x_offset) self.x_offset = offset;
         }
     }
 
-    fn deinit(self: *Puzzle_Data) void {
+    fn deinit(self: *Data) void {
         if (self.bytes.len == 0) return;
 
-        self.allocator.free(self.bytes);
+        self.ally.free(self.bytes);
 
-        for (self.row_info) |line| self.allocator.free(line);
-        self.allocator.free(self.row_info);
+        for (self.row_info) |line| self.ally.free(line);
+        self.ally.free(self.row_info);
 
-        for (self.col_info) |line| self.allocator.free(line);
-        self.allocator.free(self.col_info);
+        for (self.col_info) |line| self.ally.free(line);
+        self.ally.free(self.col_info);
 
-        for (self.grid) |line| self.allocator.free(line);
-        self.allocator.free(self.grid);
+        for (self.grid) |line| self.ally.free(line);
+        self.ally.free(self.grid);
 
         self.x_offset = 0;
         self.y_offset = 0;
-
-        for (self.row_strs) |line| self.allocator.free(line);
-        self.allocator.free(self.row_strs);
-
-        for (self.col_strs) |line| {
-            for (line) |num| self.allocator.free(num);
-            self.allocator.free(line);
-        }
-        self.allocator.free(self.col_strs);
     }
 
-    fn draw(self: Puzzle_Data, window_w: usize, window_h: usize) void {
-        _ = window_w;
-        _ = window_h;
-
+    fn draw(self: *Data) void {
         var x: c_int = self.x_offset + 4 * self.gap;
         var y: c_int = 0;
 
+        var iter = std.mem.splitScalar(u8, self.bytes, '\n');
+        _ = iter.next().?; // skip x len, y len
+        _ = iter.next().?; // skip blank line
+
         // Draw column number text
-        for (self.col_strs) |line| {
+        for (0..self.col_info.len) |_| {
             y = 0;
-            for (line) |num| {
-                rl.DrawText(num.ptr, x, y, self.size, rl.WHITE);
+            var num_it = std.mem.splitScalar(u8, iter.next().?, ' ');
+            while (num_it.next()) |num| {
+                const txt = self.bufZ(num);
+                rl.DrawText(txt, x, y, self.size, rl.WHITE);
                 y += self.size + self.gap;
             }
             x += self.size + self.gap;
         }
 
         // Draw row number text
+        _ = iter.next().?; // skip blank line
         x = 0;
         y = self.y_offset + 2 * self.gap;
-        for (self.row_strs) |line| {
-            rl.DrawText(line, x, y, self.size, rl.WHITE);
+        while (iter.next()) |line| {
+            const txt = self.bufZ(line);
+            rl.DrawText(txt, x, y, self.size, rl.WHITE);
             y += self.size + self.gap;
         }
 
+        // Draw puzzle grid
         y = self.y_offset;
         for (self.grid, 1..) |row, i| {
             x = self.x_offset;
@@ -196,5 +186,11 @@ const Puzzle_Data = struct {
             }
             y += self.size + if (i % 5 == 0) 2 * self.gap else self.gap;
         }
+    }
+
+    fn bufZ(self: *Data, text: []const u8) [:0]u8 {
+        @memcpy(self.buffer[0..text.len], text);
+        self.buffer[text.len] = 0;
+        return self.buffer[0..text.len :0];
     }
 };
