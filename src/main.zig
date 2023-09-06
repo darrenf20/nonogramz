7a8adf54e65f19dc8ca6c7da405a33efc403a69a
Darren Fielding <darren.fielding@hotmail.co.uk>
2023-09-06 16:51:09 +0100

    Began using more of a grid

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,8 @@ pub fn main() !void {
     const win_w = 800;
     const win_h = 800;
 
-    const size = @as(c_int, @intCast(@min(win_w, win_h) * 4 / 100));
+    //const size = @as(c_int, @intCast(@min(win_w, win_h) * 4 / 100));
+    const size: c_int = 50;
     const gap: c_int = @divFloor(size, 15);
     var data = Data{ .ally = ally, .size = size, .gap = gap };
     defer data.deinit();
@@ -28,7 +29,7 @@ pub fn main() !void {
 
         // Draw
         rl.BeginDrawing();
-        rl.ClearBackground(rl.DARKGRAY);
+        rl.ClearBackground(rl.WHITE);
 
         if (data.bytes.len == 0) {
             const text = "Drop puzzle file here";
@@ -41,7 +42,8 @@ pub fn main() !void {
                 rl.LIGHTGRAY,
             );
         } else {
-            data.draw();
+            //data.draw(win_w, win_h);
+            data.draw_grid_lines();
         }
 
         rl.EndDrawing();
@@ -52,13 +54,12 @@ const Data = struct {
     ally: std.mem.Allocator,
 
     bytes: []u8 = &.{},
-    buffer: [256]u8 = undefined,
+    buffer: [4]u8 = undefined,
 
     row_info: [][]usize = undefined,
     col_info: [][]usize = undefined,
     grid: [][]u1 = undefined,
 
-    // try making these usize
     size: c_int,
     gap: c_int,
     x_offset: c_int = 0,
@@ -101,11 +102,11 @@ const Data = struct {
             var len = std.mem.count(u8, str, " ") + 1;
             line.* = try self.ally.alloc(usize, len);
 
+            var offset: c_int = 0;
             for (line.*) |*sq| {
                 sq.* = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
+                offset += 1;
             }
-
-            var offset = @as(c_int, @intCast(len)) * (self.size + self.gap);
             if (offset > self.y_offset) self.y_offset = offset;
         }
 
@@ -117,12 +118,11 @@ const Data = struct {
             var len = std.mem.count(u8, str, " ") + 1;
             line.* = try self.ally.alloc(usize, len);
 
+            var offset: c_int = 0;
             for (line.*) |*sq| {
                 sq.* = try std.fmt.parseUnsigned(usize, num_it.next().?, 10);
+                offset += 1;
             }
-
-            const cstr = self.bufZ(str);
-            var offset = rl.MeasureText(cstr, self.size) + 3 * self.gap;
             if (offset > self.x_offset) self.x_offset = offset;
         }
     }
@@ -145,52 +145,113 @@ const Data = struct {
         self.y_offset = 0;
     }
 
-    fn draw(self: *Data) void {
-        var x: c_int = self.x_offset + 4 * self.gap;
-        var y: c_int = 0;
+    //fn draw(self: *Data) !void {
+    //    var x: c_int = self.x_offset + 4 * self.gap;
+    //    var y: c_int = self.gap;
 
-        var iter = std.mem.splitScalar(u8, self.bytes, '\n');
-        _ = iter.next().?; // skip x len, y len
-        _ = iter.next().?; // skip blank line
+    //    // Draw column number text
+    //    for (self.col_info) |line| {
+    //        y = self.gap;
+    //        for (line) |num| {
+    //            const txt = try self.bufZ(num);
+    //            rl.DrawText(txt, x, y, self.size, rl.BLACK);
+    //            y += self.size + self.gap;
+    //        }
+    //        x += self.size + self.gap;
+    //    }
 
-        // Draw column number text
-        for (0..self.col_info.len) |_| {
-            y = 0;
-            var num_it = std.mem.splitScalar(u8, iter.next().?, ' ');
-            while (num_it.next()) |num| {
-                const txt = self.bufZ(num);
-                rl.DrawText(txt, x, y, self.size, rl.WHITE);
-                y += self.size + self.gap;
-            }
-            x += self.size + self.gap;
-        }
+    //    // Draw row number text
+    //    x = self.gap;
+    //    for (self.row_info) |line| {
+    //        y += self.size + self.gap;
+    //        for (line) |num| {
+    //            const txt = try self.bufZ(num);
+    //            rl.DrawText(txt, x, y, self.size, rl.BLACK);
+    //            x += self.size + self.gap;
+    //        }
+    //    }
 
-        // Draw row number text
-        _ = iter.next().?; // skip blank line
-        x = 0;
-        y = self.y_offset + 2 * self.gap;
-        while (iter.next()) |line| {
-            const txt = self.bufZ(line);
-            rl.DrawText(txt, x, y, self.size, rl.WHITE);
-            y += self.size + self.gap;
-        }
+    //    // Draw puzzle grid
+    //    y = self.y_offset;
+    //    for (self.grid, 1..) |row, i| {
+    //        x = self.x_offset;
+    //        for (row, 1..) |sq, j| {
+    //            const colour = if (sq == 0) rl.WHITE else rl.BLACK;
+    //            rl.DrawRectangle(x, y, self.size, self.size, colour);
+    //            x += self.size + if (j % 5 == 0) 2 * self.gap else self.gap;
+    //        }
+    //        y += self.size + if (i % 5 == 0) 2 * self.gap else self.gap;
+    //    }
+    //}
 
-        // Draw puzzle grid
-        y = self.y_offset;
-        for (self.grid, 1..) |row, i| {
-            x = self.x_offset;
-            for (row, 1..) |sq, j| {
-                const colour = if (sq == 0) rl.WHITE else rl.BLACK;
-                rl.DrawRectangle(x, y, self.size, self.size, colour);
-                x += self.size + if (j % 5 == 0) 2 * self.gap else self.gap;
-            }
-            y += self.size + if (i % 5 == 0) 2 * self.gap else self.gap;
-        }
+    fn bufZ(self: *Data, num: usize) ![:0]u8 {
+        const slice = try std.fmt.bufPrint(self.buffer, "{}", .{num});
+        self.buffer[slice.len] = 0;
+        return self.buffer[0..slice.len :0];
     }
 
-    fn bufZ(self: *Data, text: []const u8) [:0]u8 {
-        @memcpy(self.buffer[0..text.len], text);
-        self.buffer[text.len] = 0;
-        return self.buffer[0..text.len :0];
+    fn draw_grid_lines(self: Data) void {
+        const x_min = self.x_offset * (self.size + self.gap) + self.gap;
+        const y_min = self.y_offset * (self.size + self.gap) + self.gap;
+
+        const rows = @as(c_int, @intCast(self.row_info.len));
+        const cols = @as(c_int, @intCast(self.col_info.len));
+
+        //const x_max =
+        //    (self.x_offset + cols) * (self.size + self.gap) +
+        //    @divFloor(cols, 5) * self.gap + self.gap;
+        //const y_max =
+        //    (self.y_offset + rows) * (self.size + self.gap) +
+        //    @divFloor(rows, 5) * self.gap + self.gap;
+
+        const x_max =
+            x_min +
+            cols * (self.size + self.gap) +
+            (@divFloor(cols, 5) + 1) * self.gap;
+        const y_max =
+            y_min +
+            rows * (self.size + self.gap) +
+            (@divFloor(rows, 5) + 1) * self.gap;
+
+        var x: c_int = self.gap;
+        var y: c_int = self.gap;
+
+        // Draw horizontal lines for column numbers
+        for (0..@as(usize, @intCast(self.y_offset))) |_| {
+            for (0..@as(usize, @intCast(self.gap))) |_| {
+                rl.DrawLine(x_min, y, x_max, y, rl.BLACK);
+                y += 1;
+            }
+            y += self.size;
+        }
+
+        // Draw horizontal lines for puzzle space
+        for (0..self.row_info.len + 1) |i| {
+            const thickness = if (i % 5 == 0) 2 * self.gap else self.gap;
+            for (0..@as(usize, @intCast(thickness))) |_| {
+                rl.DrawLine(self.gap, y, x_max, y, rl.BLACK);
+                y += 1;
+            }
+            y += self.size;
+        }
+
+        // Draw vertical lines for row numbers
+        for (0..@as(usize, @intCast(self.x_offset))) |_| {
+            for (0..@as(usize, @intCast(self.gap))) |_| {
+                rl.DrawLine(x, y_min, x, y_max, rl.BLACK);
+                x += 1;
+            }
+            x += self.size;
+        }
+
+        // Draw vertical lines for puzzle space
+        for (0..self.col_info.len + 1) |i| {
+            const thickness = if (i % 5 == 0) 2 * self.gap else self.gap;
+            for (0..@as(usize, @intCast(thickness))) |_| {
+                rl.DrawLine(x, self.gap, x, y_max, rl.BLACK);
+                x += 1;
+            }
+            x += self.size;
+        }
     }
 };
