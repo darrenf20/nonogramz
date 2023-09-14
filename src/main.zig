4ea9e8e9b095186a7522b7f34f33236aa3f66099
Darren Fielding <darren.fielding@hotmail.co.uk>
2023-09-14 11:42:43 +0100

    Add nCr function

--- src/main.zig
+++ src/main.zig
@@ -407,3 +407,13 @@ const Drawer = struct {
         }
     }
 };
+
+fn nCr(n: usize, r: usize) usize {
+    const k = if (r > n - r) n - r else r;
+    var result: usize = 1;
+    for (1..k + 1) |i| {
+        result *= n - k + 1;
+        result /= i;
+    }
+    return result;
+}
